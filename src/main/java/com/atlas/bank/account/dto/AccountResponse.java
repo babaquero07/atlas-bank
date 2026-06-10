cf08b42fd69898c337f30ca15e84d3ff1d14a47b
Alexander Baquero <brayanalexanderb@gmail.com>
2026-06-10 13:04:24 -0500

    Add DTO classes for account and transaction models; enhance Account model with explicit equals and hashCode methods

---
+++ src/main/java/com/atlas/bank/account/dto/AccountResponse.java
@@ -0,0 +1,18 @@
+package com.atlas.bank.account.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class AccountResponse {
+    private Long id;
+    private String accountNumber;
+    private String ownerName;
+    private String email;
+    private String type;
+    private BigDecimal balance;
+    private String status;
+    private LocalDateTime createdDate;
+}
--- src/main/java/com/atlas/bank/account/model/Account.java
+++ src/main/java/com/atlas/bank/account/model/Account.java
@@ -1,21 +1,21 @@
 package com.atlas.bank.account.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//@Table(name = "accounts")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String accountNumber;
     private String ownerName;
