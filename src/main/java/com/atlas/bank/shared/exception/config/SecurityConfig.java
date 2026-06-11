1bb72d6516cb52611fb228329804341ee1ef9647
Alexander Baquero <brayanalexanderb@gmail.com>
2026-06-11 13:17:37 -0500

    Refactor `SecurityConfig`; remove unused imports, adjust formatting, and simplify method signatures

--- src/main/java/com/atlas/bank/shared/exception/config/SecurityConfig.java
+++ src/main/java/com/atlas/bank/shared/exception/config/SecurityConfig.java
@@ -2,15 +2,12 @@ package com.atlas.bank.shared.exception.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,52 +17,52 @@ import java.util.stream.Collectors;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests(auth -> auth
-                        // Accounts
-                        .requestMatchers(HttpMethod.POST, "/api/v1/accounts").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/v1/accounts").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/v1/accounts/{id}").hasAnyRole("ADMIN", "USER")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(  auth -> auth
+                        //Accounts
+                        .requestMatchers( HttpMethod.POST, "/api/v1/accounts" ).hasRole("ADMIN")
+                        .requestMatchers( HttpMethod.GET, "/api/v1/accounts" ).hasRole("ADMIN")
+                        .requestMatchers( HttpMethod.GET, "/api/v1/accounts/{id}" ).hasAnyRole("USER","ADMIN")
 
-                        // Transactions
-                        .requestMatchers(HttpMethod.POST, "/api/v1/transactions").hasAnyRole("ADMIN", "USER")
-                        .requestMatchers(HttpMethod.GET, "/api/v1/transactions/{id}/transactions").hasAnyRole("ADMIN", "USER")
+                        //Transactions
+                        .requestMatchers(HttpMethod.POST, "/api/v1/transactions/transfer")
+                        .hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/v1/transactions/{id}/transactions")
+                        .hasAnyRole("USER", "ADMIN")
 
-                        // H2
+                        //H2
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
-
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())
                         )
                 )
                 .headers(headers ->
-                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-                )
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .csrf(AbstractHttpConfigurer::disable);
 
-        return httpSecurity.build();
+        return http.build();
     }
 
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    private JwtAuthenticationConverter jwtAuthenticationConverter(){
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
 
-        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+        converter.setJwtGrantedAuthoritiesConverter( jwt -> {
             var realmAccess = jwt.getClaimAsMap("realm_access");
-            if (realmAccess == null || realmAccess.get("roles") == null) {
+            if(realmAccess==null || realmAccess.get("roles")==null){
                 return List.of();
             }
-
             var roles = (List<String>) realmAccess.get("roles");
 
             return roles.stream()
-                    .map(SimpleGrantedAuthority::new)
+                    .map(role -> new SimpleGrantedAuthority(role))
                     .collect(Collectors.toList());
-        });
+        } );
 
         return converter;
     }
