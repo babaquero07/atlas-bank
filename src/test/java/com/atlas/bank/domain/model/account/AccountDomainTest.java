c15a571b08e6a99025c327ae8d1ce0cb7a3b29b1
Alexander Baquero <brayanalexanderb@gmail.com>
2026-06-22 15:59:17 -0500

    Testing del dominio pt 2

--- src/test/java/com/atlas/bank/domain/model/account/AccountDomainTest.java
+++ src/test/java/com/atlas/bank/domain/model/account/AccountDomainTest.java
@@ -1,5 +1,6 @@
 package com.atlas.bank.domain.model.account;
 
+import com.atlas.bank.domain.exception.InsufficientFundsException;
 import com.atlas.bank.domain.model.shared.Currency;
 import com.atlas.bank.domain.model.shared.Email;
 import com.atlas.bank.domain.model.shared.Money;
@@ -36,12 +37,48 @@ class AccountDomainTest {
 
     @Test
     @DisplayName("Retirar dinero con fondos suficientes")
-    void shouldRetireMoney() {
+    void shouldWithdrawWhenSufficientFunds() {
         Account account = createAccountWithBalance(new BigDecimal(1000));
         Money withdrawal = Money.of(new BigDecimal(300), Currency.USD);
 
         account.withdraw(withdrawal);
 
         assertEquals(Money.of(new BigDecimal("700.00"), Currency.USD), account.getBalance());
+    }
+
+    @Test
+    @DisplayName("Lanzar exepcion con fondos insuficientes")
+    void shouldRejectWithdrawWhenInsufficientFunds() {
+        Account account = createAccountWithBalance(new BigDecimal(1000));
+        Money withdrawal = Money.of(new BigDecimal(1500), Currency.USD);
+
+        assertThrows(InsufficientFundsException.class, () -> account.withdraw(withdrawal));
+    }
+
+    @Test
+    @DisplayName("Reject deposit if the amount is negative")
+    void shouldRejectDepositIfAmountIsNegative() {
+        Account account = createAccountWithBalance(new BigDecimal(1000));
+        Money deposit = Money.of(new BigDecimal(-100), Currency.USD);
+
+        assertThrows(IllegalArgumentException.class, () -> account.deposit(deposit));
+    }
+
+    @Test
+    @DisplayName("Debe inicializar valores por defecto")
+    void shouldInitializeAccountWithBalance() {
+        Account account = Account
+                .builder()
+                .accountNumber("ATL-0001-0001-0001")
+                .ownerName("Alexander Baquero")
+                .email(Email.of("abaquero@gmail.com"))
+                .type(AccountType.SAVINGS)
+                .build();
+
+        account.initDefaults();
+
+        assertEquals(AccountStatus.ACTIVE, account.getStatus());
+        assertEquals(Money.zero(Currency.ARS), account.getBalance());
+        assertNotNull(account.getCreatedAt());
     }
 }
